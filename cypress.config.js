e511d7f870450d7706d91ab1c4db69a131beacae
Officialsummit <xettrisumit@gmail.com>
2025-03-02 18:29:17 +1300

    add cypress-mochawesome-reporter to add html report for testcases after they finish running

--- cypress.config.js
+++ cypress.config.js
@@ -2,15 +2,23 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   defaultCommandTimeout: 6000,
-  
+  reporter:'cypress-mochawesome-reporter',
+  reporterOptions:{
+    charts:true,
+  },
+  video:true,
   env:{    
     base_url:'https://rahulshettyacademy.com',
     login_url:"/loginpagePractise/",
     homepage_url:"/angularpractice/shop" 
   },
   e2e: {
-    setupNodeEvents(on, config) {
+      setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
+
+      
+
     },
 
   },
